1b263ae2e99db38d04912760cded5b8161c8c849
Abubaker1030 <abubakerashiq77@gmail.com>
2021-11-15 18:57:14 +0500

    first commit

--- textUtils/views.py
+++ textUtils/views.py
@@ -9,31 +9,29 @@ def index(request):
     return render(request,'home.html',info)
 
 def analyzetext(request):
-    text = request.GET.get('text', 'default')
-    removepunc = request.GET.get('removepunc', 'off')
-    uppercase = request.GET.get('uppercase','off')
-    capatilize = request.GET.get('capatilize','off')
-    spaceremover = request.GET.get('spaceremover','off')
-    count = request.GET.get('cpunt','off')
-    if removepunc=='on':
-        analyzedText = ""
-        for char in text:
-            if char not in string.punctuation:
-                analyzedText = analyzedText + char
-        text=analyzedText
-    if uppercase=='on':
-        text = text.upper()
-    if capatilize=='on' and uppercase=='off':
-        text = text.capitalize()
-    if spaceremover=='on':
-        spaceremoved = ""
-        for char in text:
-            if char!=' ':
-                spaceremoved = spaceremoved+char
-        text = spaceremoved
+    text = request.POST.get('text', 'default')
+    removepunc = request.POST.get('removepunc', 'off')
+    uppercase = request.POST.get('uppercase','off')
+    capatilize = request.POST.get('capatilize','off')
+    spaceremover = request.POST.get('spaceremover','off')
+    if spaceremover=='on' or removepunc=='on' or capatilize=='on' or uppercase=='on':
+        if removepunc == 'on':
+            analyzedText = ""
+            for char in text:
+                if char not in string.punctuation:
+                    analyzedText = analyzedText + char
+            text = analyzedText
+        if uppercase == 'on':
+            text = text.upper()
+        if capatilize == 'on' and uppercase == 'off':
+            text = text.capitalize()
+        if spaceremover == 'on':
+            spaceremoved = ""
+            for char in text:
+                if char != ' ':
+                    spaceremoved = spaceremoved + char
+            text = spaceremoved
+    else:
+        text = "No Operation Selected"
     dict = {'analyezedText':text}
-    if count=='on':
-        counter = "Total letters are" + len(text)
-        print(counter)
-        dict['count'] = counter
     return render(request,'analyzetext.html',dict)
